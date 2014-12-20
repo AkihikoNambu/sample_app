bc2313731c7d08fdb8bdc5c739cafc5078333423
Akihiko Nambu <mate0rich.nmb@gmail.com>
2014-12-20 17:30:43 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
+  #デフォルトではbefore_actionが全てのアクションに適用される。
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
   def new
   	@user = User.new
+  end
+
+  def index
+  	# @users = User.all
+
+  	@users = User.paginate(page: params[:page])
   end
 
   def show
@@ -18,12 +29,45 @@ class UsersController < ApplicationController
 	end
   end
 
-  private
+  def edit
+  	# beforeアクションで以下のコードを実行済み
+    # @user = User.find(params[:id])
+  end
+
+  def update
+  	# beforeアクションで以下を実行済み
+    # @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
 
   private
-
+  	#ストロングパラメータ
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+    # Before actions
+    def signed_in_user
+      unless signed_in?
+        store_location
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+   end
 end
